95db6ee55e48cd1f863ccf65833b4f06628f3295
rochaxtwo <oscrocha167@csu.fullerton.edu>
2016-12-08 14:19:32 -0800

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -6,12 +6,28 @@ public:
 	// TO DO
 	// initialize an undirected graph that can store at most n vertices
 	Graph(const int n) {
+		this->n = n;
+		visited = new bool[n];
+		ptr = new int*[n];
 
+		for (int i = 0; i < n; i++) {
+			ptr[i] = new int[n];
+			for (int j = 0; j < n; j++) {
+				ptr[i][j] = 0;
+			}
+		}
 	}
 
 	// TO DO
 	// insert an edge between vertices u and v
 	void insertEdge(int u, int v) {
+		if (u > n || v > n || u < 0 || n < 0) {
+			cout << " Invalid edge! " << endl;
+		}
+		else
+		{
+			ptr[u - 1][v - 1] = 1;
+		}
 
 	}
 
@@ -35,5 +51,7 @@ public:
 private:
 	// TO DO
 	// member variables and functions to implement the public member functions
-
+	int **ptr;
+	int n;
+	bool *visited;
 };
